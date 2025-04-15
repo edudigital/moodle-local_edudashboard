5c2315b5b01f257d2e71a7a1be5b4c81bb72ce8d
admilsonP <admilsonpina18@gmail.com>
2025-04-15 14:26:51 -0100

    named constants

--- classes/constants.php
+++ classes/constants.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- *
+ * Defines all constants for use in the EDUdashboard plugin.
  *
  * @package     local_edudashboard
  * @category    admin
@@ -23,67 +23,66 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-/**
- * Define all constants for use
- */
+/* Course completion constants */
+define('LOCAL_EDUDASHBOARD_COURSE_COMPLETE_00PER', 0);
+define('LOCAL_EDUDASHBOARD_COURSE_COMPLETE_20PER', 0.2);
+define('LOCAL_EDUDASHBOARD_COURSE_COMPLETE_40PER', 0.4);
+define('LOCAL_EDUDASHBOARD_COURSE_COMPLETE_60PER', 0.6);
+define('LOCAL_EDUDASHBOARD_COURSE_COMPLETE_80PER', 0.8);
+define('LOCAL_EDUDASHBOARD_COURSE_COMPLETE_100PER', 1);
 
-/* Course completion constant */
-define('LOCAL_SITEREPORT_COURSE_COMPLETE_00PER', 0);
-define('LOCAL_SITEREPORT_COURSE_COMPLETE_20PER', 0.2);
-define('LOCAL_SITEREPORT_COURSE_COMPLETE_40PER', 0.4);
-define('LOCAL_SITEREPORT_COURSE_COMPLETE_60PER', 0.6);
-define('LOCAL_SITEREPORT_COURSE_COMPLETE_80PER', 0.8);
-define('LOCAL_SITEREPORT_COURSE_COMPLETE_100PER', 1);
+/* Percentage constants */
+define('LOCAL_EDUDASHBOARD_PERCENTAGE_00', "0%");
+define('LOCAL_EDUDASHBOARD_PERCENTAGE_20', "20%");
+define('LOCAL_EDUDASHBOARD_PERCENTAGE_40', "40%");
+define('LOCAL_EDUDASHBOARD_PERCENTAGE_60', "60%");
+define('LOCAL_EDUDASHBOARD_PERCENTAGE_80', "80%");
+define('LOCAL_EDUDASHBOARD_PERCENTAGE_100', "100%");
 
-/* Percentage constant */
-define('LOCAL_SITEREPORT_PERCENTAGE_00', "0%");
-define('LOCAL_SITEREPORT_PERCENTAGE_20', "20%");
-define('LOCAL_SITEREPORT_PERCENTAGE_40', "40%");
-define('LOCAL_SITEREPORT_PERCENTAGE_60', "60%");
-define('LOCAL_SITEREPORT_PERCENTAGE_80', "80%");
-define('LOCAL_SITEREPORT_PERCENTAGE_100', "100%");
+/* Time constants */
+define('LOCAL_EDUDASHBOARD_ONEDAY', 24 * 60 * 60);
+define('LOCAL_EDUDASHBOARD_ONEWEEK', 7 * 24 * 60 * 60);
+define('LOCAL_EDUDASHBOARD_ONEMONTH', 30 * 24 * 60 * 60);
+define('LOCAL_EDUDASHBOARD_ONEYEAR', 365 * 24 * 60 * 60);
+define('LOCAL_EDUDASHBOARD_ALL', "all");
+define('LOCAL_EDUDASHBOARD_WEEKLY', "weekly");
+define('LOCAL_EDUDASHBOARD_MONTHLY', "monthly");
+define('LOCAL_EDUDASHBOARD_YEARLY', "yearly");
+define('LOCAL_EDUDASHBOARD_WEEKLY_DAYS', 7);
+define('LOCAL_EDUDASHBOARD_MONTHLY_DAYS', 30);
+define('LOCAL_EDUDASHBOARD_YEARLY_DAYS', 365);
 
-/* Time constant */
-define('LOCAL_SITEREPORT_ONEDAY', 24 * 60 * 60);
-define('LOCAL_SITEREPORT_ONEWEEK', 7 * 24 * 60 * 60);
-define('LOCAL_SITEREPORT_ONEMONTH', 30 * 24 * 60 * 60);
-define('LOCAL_SITEREPORT_ONEYEAR', 365 * 24 * 60 * 60);
-define('LOCAL_SITEREPORT_ALL', "all");
-define('LOCAL_SITEREPORT_WEEKLY', "weekly");
-define('LOCAL_SITEREPORT_MONTHLY', "monthly");
-define('LOCAL_SITEREPORT_YEARLY', "yearly");
-define('LOCAL_SITEREPORT_WEEKLY_DAYS', 7);
-define('LOCAL_SITEREPORT_MONTHLY_DAYS', 30);
-define('LOCAL_SITEREPORT_YEARLY_DAYS', 365);
+/* Email schedule constants */
+define('LOCAL_EDUDASHBOARD_ESR_DAILY_EMAIL', 0);
+define('LOCAL_EDUDASHBOARD_ESR_WEEKLY_EMAIL', 1);
+define('LOCAL_EDUDASHBOARD_ESR_MONTHLY_EMAIL', 2);
 
-define('LOCAL_SITEREPORT_ESR_DAILY_EMAIL', 0);
-define('LOCAL_SITEREPORT_ESR_LOCAL_SITEREPORT_WEEKLY_EMAIL', 1);
-define('LOCAL_SITEREPORT_ESR_LOCAL_SITEREPORT_MONTHLY_EMAIL', 2);
+/* Email time constants */
+define('LOCAL_EDUDASHBOARD_ESR_0630AM', 0);
+define('LOCAL_EDUDASHBOARD_ESR_1000AM', 1);
+define('LOCAL_EDUDASHBOARD_ESR_0430PM', 2);
+define('LOCAL_EDUDASHBOARD_ESR_1030PM', 3);
 
-define('LOCAL_SITEREPORT_ESR_0630AM', 0);
-define('LOCAL_SITEREPORT_ESR_1000AM', 1);
-define('LOCAL_SITEREPORT_ESR_0430PM', 2);
-define('LOCAL_SITEREPORT_ESR_1030PM', 3);
+/* Block type constants */
+define('LOCAL_EDUDASHBOARD_BLOCK_TYPE_DEFAULT', 0);
+define('LOCAL_EDUDASHBOARD_BLOCK_TYPE_CUSTOM', 1);
 
-// Define block type.
-define('LOCAL_SITEREPORT_BLOCK_TYPE_DEFAULT', 0);
-define('LOCAL_SITEREPORT_BLOCK_TYPE_CUSTOM', 1);
+/* Block view constants */
+define('LOCAL_EDUDASHBOARD_BLOCK_DESKTOP_VIEW', 'desktopview');
+define('LOCAL_EDUDASHBOARD_BLOCK_TABLET_VIEW', 'tabletview');
+define('LOCAL_EDUDASHBOARD_BLOCK_LARGE', 2);
+define('LOCAL_EDUDASHBOARD_BLOCK_MEDIUM', 1);
+define('LOCAL_EDUDASHBOARD_BLOCK_SMALL', 0);
 
-// Block View.
-define('LOCAL_SITEREPORT_BLOCK_DESKTOP_VIEW', 'desktopview');
-define('LOCAL_SITEREPORT_BLOCK_TABLET_VIEW', 'tabletview');
-define('LOCAL_SITEREPORT_BLOCK_LARGE', 2);
-define('LOCAL_SITEREPORT_BLOCK_MEDIUM', 1);
-define('LOCAL_SITEREPORT_BLOCK_SMALL', 0);
+/* Course Progress Manager constants */
+define('LOCAL_EDUDASHBOARD_CPM_STUDENTS_ARCHETYPE', 'student');
 
-// Course Progres Manager.
-define('CPM_STUDENTS_ARCHETYPE', 'student');
-
-// Color Themes.
+/* Color themes constant */
 define('LOCAL_EDUDASHBOARD_COLOR_THEMES', [
     ['#F98012', '#133F3F', '#00A1A8', '#444444', '#666666'],
     ['#AC0034', '#142458', '#DFC11C', '#333333', '#999999'],
     ['#ED553B', '#20639B', '#92CD53', '#222222', '#888888'],
 ]);
 
-define('UPGRADE_URL', 'https://bit.ly/3rJ9l24');
+/* Upgrade URL constant */
+define('LOCAL_EDUDASHBOARD_UPGRADE_URL', 'https://edudigital.pt');
